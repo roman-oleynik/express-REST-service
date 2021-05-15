8f30fa0a2970c83cc1a974dd48aee4afcfe446fa
roman-oleynik <raman.alejnik@gmail.com>
2021-05-15 23:50:20 +0300

    feat: add task deletion feature while deleting the board

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,33 @@
-const getAll = async () => {
+const { users, tasks } = require("../../data/data");
+
+const getAll = async () => 
   // TODO: mock implementation. should be replaced during task development
-  return [];
+   [...users]
+;
+
+const get = id => users.find(el => el.id === id);
+
+const add = async user => {
+  users.push(user);
+  return user;
 };
 
-module.exports = { getAll };
+const put = async (id, user) => {
+  const indexOfUserById = users.findIndex(el => el.id === id);
+  users[indexOfUserById] = user;
+  return user;
+};
+
+const remove = async id => {
+  const indexOfUserById = users.findIndex(el => el.id === id);
+  users.splice(indexOfUserById, 1);
+  tasks.forEach(el => {
+    if (el.userId === id) {
+      el.userId = null;
+    }
+    return el;
+  })
+  return {};
+};
+
+module.exports = { getAll, get, add, put, remove };
